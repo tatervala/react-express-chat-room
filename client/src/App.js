7a9188c77821d7e962b6a36aae116aa390de8b88
tatervala <94960436+tatervala@users.noreply.github.com>
2023-03-08 22:06:36 +0200

    .env

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,11 @@ import JoinPage from './pages/joinpage'
 import Chats from './pages/chatpage'
 import NaviBar from './components/Header'
 
-const socket = io.connect('http://localhost:4000/') // socket server
+
+let URI
+process.env.REACT_APP_STATUS === 'development' ? (URI = process.env.REACT_APP_DEV_URI) :
+ (URI = process.env.REACT_APP_PROD_URI)
+const socket = io.connect(URI) // socket server
 
 
 function App() {
